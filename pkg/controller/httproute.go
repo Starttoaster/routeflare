e9e5356bdd3d0e3fdb34dab2acc132a6ba84ba19
Starttoaster <b.butler@chia.net>
2025-12-02 15:15:14 -0800

    Define better record creation/update logic in cases where parent Gateway contains multiple addresses

---
+++ pkg/controller/httproute.go
@@ -0,0 +1,470 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/starttoaster/routeflare/pkg/cloudflare"
+	"github.com/starttoaster/routeflare/pkg/gateway"
+	"github.com/starttoaster/routeflare/pkg/kubernetes"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"log"
+	"time"
+)
+
+// watchHTTPRoutes watches for HTTPRoute changes
+func (c *Controller) watchHTTPRoutes() error {
+	for {
+		watcher, err := c.k8sClient.WatchHTTPRoutes(c.ctx)
+		if err != nil {
+			return fmt.Errorf("error watching HTTPRoutes: %w", err)
+		}
+
+		log.Println("HTTPRoute watcher started. Waiting for events...")
+
+		for {
+			done, err := c.watchHTTPRoutesEventLoop(watcher)
+			if err != nil {
+				log.Printf("Watcher error: %v", err)
+				time.Sleep(5 * time.Second)
+				break // breaks out of inner loop to attempt a reconnect
+			}
+			if done {
+				return nil // breaks out of both loops if routeflare is shutting down
+			}
+		}
+	}
+}
+
+func (c *Controller) watchHTTPRoutesEventLoop(watcher watch.Interface) (bool, error) {
+	select {
+	case <-c.ctx.Done():
+		log.Println("Context cancelled, stopping watcher")
+		watcher.Stop()
+		return true, nil
+	case event, ok := <-watcher.ResultChan():
+		if !ok {
+			watcher.Stop()
+			return false, fmt.Errorf("watch channel closed, will attempt to restart watcher")
+		}
+
+		switch event.Type {
+		case watch.Added:
+			// All HTTPRoutes will appear to be Added when the watcher first starts up
+			if route, ok := event.Object.(*unstructured.Unstructured); ok {
+				log.Printf("HTTPRoute added: %s/%s", route.GetNamespace(), route.GetName())
+				c.processHTTPRoute(route, false)
+			}
+		case watch.Modified:
+			if route, ok := event.Object.(*unstructured.Unstructured); ok {
+				log.Printf("HTTPRoute modified: %s/%s", route.GetNamespace(), route.GetName())
+				c.processHTTPRoute(route, false)
+			}
+		case watch.Deleted:
+			log.Printf("HTTPRoute deleted: %s/%s", getNamespace(event.Object), getName(event.Object))
+			c.processHTTPRouteDeletion(event.Object)
+		case watch.Error:
+			log.Printf("Watch error: %v", event.Object)
+		}
+	}
+	return false, nil
+}
+
+// processHTTPRoute processes a single HTTPRoute
+func (c *Controller) processHTTPRoute(route *unstructured.Unstructured, isDDNSUpdate bool) {
+	name, namespace, annotations, err := kubernetes.ExtractHTTPRouteMetadata(route)
+	if err != nil {
+		log.Printf("Error extracting metadata from HTTPRoute: %v", err)
+		return
+	}
+
+	// Extract routeflare annotations
+	routeflareAnns := extractRouteflareAnnotations(annotations)
+	if len(routeflareAnns) == 0 {
+		return // No routeflare annotations, skip
+	}
+
+	// Check for required content-mode annotation
+	contentMode, ok := routeflareAnns["content-mode"]
+	if !ok || contentMode == "" {
+		return // No content-mode, skip
+	}
+
+	// Get record name from HTTPRoute spec.hostnames
+	recordName, err := getRecordNameFromHTTPRoute(route)
+	if err != nil {
+		log.Printf("Error getting record name from HTTPRoute %s/%s: %v", namespace, name, err)
+		return
+	}
+
+	// Get zone name from HTTPRoute
+	// TODO: any downsides to the zone being derived from the HTTPRoute? Should zone be an annotation? For now this seems sufficient.
+	zoneName, err := extractZoneFromRecordName(recordName)
+	if err != nil {
+		log.Printf("Error extracting zone from record name %s for HTTPRoute %s/%s: %v", recordName, namespace, name, err)
+		return
+	}
+
+	// Parse other annotations
+	recordType := routeflareAnns["type"]
+	if recordType == "" {
+		recordType = "A" // Default to A
+	}
+
+	ttl, err := cloudflare.ParseTTL(routeflareAnns["ttl"])
+	if err != nil {
+		log.Printf("Error parsing TTL for HTTPRoute %s/%s: %v", namespace, name, err)
+		ttl = 1 // Default to auto
+	}
+
+	proxied, err := cloudflare.ParseProxied(routeflareAnns["proxied"])
+	if err != nil {
+		log.Printf("Error parsing proxied for HTTPRoute %s/%s: %v", namespace, name, err)
+		proxied = false
+	}
+
+	// Process based on content mode
+	switch contentMode {
+	case "gateway-address":
+		c.processGatewayAddressMode(route, zoneName, recordName, recordType, ttl, proxied)
+	case "ddns":
+		c.processDDNSMode(route, namespace, name, zoneName, recordName, recordType, ttl, proxied, isDDNSUpdate)
+	default:
+		log.Printf("Unknown content-mode '%s' for HTTPRoute %s/%s", contentMode, namespace, name)
+	}
+}
+
+// processGatewayAddressMode processes HTTPRoute with gateway-address content mode
+func (c *Controller) processGatewayAddressMode(route *unstructured.Unstructured, zoneName, recordName, recordType string, ttl int, proxied bool) {
+	// Get parent Gateway references
+	parents, found, err := unstructured.NestedSlice(route.Object, "spec", "parentRefs")
+	if !found || err != nil || len(parents) == 0 {
+		log.Printf("HTTPRoute %s/%s has no parentRefs", route.GetNamespace(), route.GetName())
+		return
+	}
+
+	// Get the first parent Gateway
+	// TODO: I don't think multiple Gateways can ever be supported by this tool, however, we may not be able to assume that the first object in the parentRefs is a Gateway.
+	// This logic may need to be built out to find the first actual Gateway parent in this list, or else another parent API object that references a Gateway itself.
+	parentRef, ok := parents[0].(map[string]interface{})
+	if !ok {
+		log.Printf("Invalid parentRef format for HTTPRoute %s/%s", route.GetNamespace(), route.GetName())
+		return
+	}
+
+	gatewayName, found, err := unstructured.NestedString(parentRef, "name")
+	if !found || err != nil {
+		log.Printf("Could not get gateway name from parentRef for HTTPRoute %s/%s", route.GetNamespace(), route.GetName())
+		return
+	}
+
+	gatewayNamespace, found, err := unstructured.NestedString(parentRef, "namespace")
+	if !found || err != nil {
+		gatewayNamespace = route.GetNamespace() // Default to HTTPRoute namespace
+	}
+
+	// Get the Gateway
+	gatewayObj, err := c.k8sClient.GetGateway(c.ctx, gatewayNamespace, gatewayName)
+	if err != nil {
+		log.Printf("Error getting Gateway %s/%s: %v", gatewayNamespace, gatewayName, err)
+		return
+	}
+
+	// Extract IP addresses from Gateway
+	ips, err := gateway.GetGatewayAddresses(gatewayObj, recordType)
+	if err != nil {
+		log.Printf("Error getting Gateway addresses: %v", err)
+		return
+	}
+
+	// Get zone ID
+	zoneID, err := c.cfClient.GetZoneIDByName(zoneName)
+	if err != nil {
+		log.Printf("Error getting zone ID for %s: %v", zoneName, err)
+		return
+	}
+
+	// Create/update DNS records
+	err = c.createOrUpdateRecords(recordType, zoneID, ips, recordName, ttl, proxied)
+	if err != nil {
+		log.Printf("Error creating or updating records: %v", err)
+	}
+}
+
+// processDDNSMode processes HTTPRoute with ddns content mode
+func (c *Controller) processDDNSMode(route *unstructured.Unstructured, namespace, name, zoneName, recordName, recordType string, ttl int, proxied bool, isDDNSUpdate bool) {
+	// Get current public IPs
+	ips, err := c.ddnsDetector.GetPublicIPsByType(c.ctx, recordType)
+	if err != nil {
+		log.Printf("Error getting public IPs for HTTPRoute %s/%s: %v", namespace, name, err)
+		return
+	}
+
+	// Check if IPs have changed (only for updates, not initial processing)
+	routeKey := fmt.Sprintf("%s/%s", namespace, name)
+	if isDDNSUpdate {
+		c.ddnsMutex.RLock()
+		ddnsRoute, exists := c.ddnsRoutes[routeKey]
+		c.ddnsMutex.RUnlock()
+
+		if exists && ipsEqual(ddnsRoute.lastIPs, ips) {
+			return // IPs haven't changed, skip update
+		}
+	}
+
+	// Get zone ID
+	zoneID, err := c.cfClient.GetZoneIDByName(zoneName)
+	if err != nil {
+		log.Printf("Error getting zone ID for %s: %v", zoneName, err)
+		return
+	}
+
+	// Create/update DNS records
+	err = c.createOrUpdateRecords(recordType, zoneID, ips, recordName, ttl, proxied)
+	if err != nil {
+		log.Printf("Error creating or updating records: %v", err)
+	}
+
+	// Store DDNS route info
+	c.ddnsMutex.Lock()
+	c.ddnsRoutes[routeKey] = &ddnsRoute{
+		namespace:  namespace,
+		name:       name,
+		zoneName:   zoneName,
+		recordName: recordName,
+		recordType: recordType,
+		ttl:        ttl,
+		proxied:    proxied,
+		lastIPs:    ips,
+	}
+	c.ddnsMutex.Unlock()
+}
+
+func (c *Controller) createOrUpdateRecords(recordType string, zoneID string, ips []string, recordName string, ttl int, proxied bool) error {
+	if len(ips) == 0 {
+		return fmt.Errorf("no IP addresses found for record type %s", recordType)
+	}
+
+	switch recordType {
+	case "A/AAAA":
+		var createdIPv4 bool
+		var createdIPv6 bool
+		for _, ip := range ips {
+			var recordTypeForIP string
+			if isIPv6(ip) {
+				recordTypeForIP = "AAAA"
+			}
+			if isIPv4(ip) {
+				recordTypeForIP = "A"
+			}
+			if recordTypeForIP == "" {
+				log.Printf("Skipping invalid IP, could not determine IP version for address %s", ip)
+				continue
+			}
+
+			record := cloudflare.DNSRecord{
+				Type:    cloudflare.RecordType(recordTypeForIP),
+				Name:    recordName,
+				Content: ip,
+				TTL:     ttl,
+				Proxied: proxied,
+			}
+
+			_, err := c.cfClient.UpsertRecord(c.ctx, zoneID, record)
+			if err != nil {
+				log.Printf("Error upserting %s record %s: %v", recordTypeForIP, recordName, err)
+				continue
+			} else {
+				log.Printf("Successfully upserted %s record %s -> %s", recordTypeForIP, recordName, ip)
+				if recordTypeForIP == "A" {
+					createdIPv4 = true
+				}
+				if recordTypeForIP == "AAAA" {
+					createdIPv6 = true
+				}
+			}
+
+			// Break out of loop if we've created a record for IPv4 and IPv6
+			if createdIPv4 && createdIPv6 {
+				break
+			}
+		}
+	case "AAAA":
+		for _, ip := range ips {
+			if isIPv6(ip) {
+				record := cloudflare.DNSRecord{
+					Type:    cloudflare.RecordType(recordType),
+					Name:    recordName,
+					Content: ip,
+					TTL:     ttl,
+					Proxied: proxied,
+				}
+
+				_, err := c.cfClient.UpsertRecord(c.ctx, zoneID, record)
+				if err != nil {
+					log.Printf("Error upserting record %s: %v", recordName, err)
+				} else {
+					log.Printf("Successfully upserted %s record %s -> %s", recordType, recordName, ip)
+				}
+				return nil
+			}
+		}
+	case "A":
+		for _, ip := range ips {
+			if isIPv4(ip) {
+				record := cloudflare.DNSRecord{
+					Type:    cloudflare.RecordType(recordType),
+					Name:    recordName,
+					Content: ip,
+					TTL:     ttl,
+					Proxied: proxied,
+				}
+
+				_, err := c.cfClient.UpsertRecord(c.ctx, zoneID, record)
+				if err != nil {
+					log.Printf("Error upserting record %s: %v", recordName, err)
+				} else {
+					log.Printf("Successfully upserted %s record %s -> %s", recordType, recordName, ip)
+				}
+				return nil
+			}
+		}
+	}
+
+	return nil
+}
+
+// processHTTPRouteDeletion handles HTTPRoute deletion
+func (c *Controller) processHTTPRouteDeletion(obj runtime.Object) {
+	if !c.cfg.ShouldDelete() {
+		return // Upsert-only strategy, don't delete
+	}
+
+	name, namespace, annotations, err := kubernetes.ExtractHTTPRouteMetadata(obj)
+	if err != nil {
+		log.Printf("Error extracting metadata from deleted HTTPRoute: %v", err)
+		return
+	}
+
+	routeflareAnns := extractRouteflareAnnotations(annotations)
+	if len(routeflareAnns) == 0 {
+		return
+	}
+
+	// Get record name
+	route, ok := obj.(*unstructured.Unstructured)
+	if !ok {
+		log.Printf("Could not convert deleted object to unstructured")
+		return
+	}
+
+	recordName, err := getRecordNameFromHTTPRoute(route)
+	if err != nil {
+		log.Printf("Error getting record name from deleted HTTPRoute %s/%s: %v", namespace, name, err)
+		return
+	}
+
+	zoneName, err := extractZoneFromRecordName(recordName)
+	if err != nil {
+		log.Printf("Error extracting zone from record name %s: %v", recordName, err)
+		return
+	}
+
+	recordType := routeflareAnns["type"]
+	if recordType == "" {
+		recordType = "A"
+	}
+
+	// Get zone ID
+	zoneID, err := c.cfClient.GetZoneIDByName(zoneName)
+	if err != nil {
+		log.Printf("Error getting zone ID for %s: %v", zoneName, err)
+		return
+	}
+
+	// Delete DNS records
+	if recordType == "A/AAAA" {
+		// Delete both A and AAAA records
+		for _, rt := range []string{"A", "AAAA"} {
+			record, err := c.cfClient.FindRecord(c.ctx, zoneID, recordName, cloudflare.RecordType(rt))
+			if err != nil {
+				log.Printf("Error finding %s record %s: %v", rt, recordName, err)
+				continue
+			}
+			if record != nil {
+				if err := c.cfClient.DeleteRecord(c.ctx, zoneID, record.ID); err != nil {
+					log.Printf("Error deleting %s record %s: %v", rt, recordName, err)
+				} else {
+					log.Printf("Successfully deleted %s record %s", rt, recordName)
+				}
+			}
+		}
+	} else {
+		record, err := c.cfClient.FindRecord(c.ctx, zoneID, recordName, cloudflare.RecordType(recordType))
+		if err != nil {
+			log.Printf("Error finding record %s: %v", recordName, err)
+			return
+		}
+		if record != nil {
+			if err := c.cfClient.DeleteRecord(c.ctx, zoneID, record.ID); err != nil {
+				log.Printf("Error deleting record %s: %v", recordName, err)
+			} else {
+				log.Printf("Successfully deleted %s record %s", recordType, recordName)
+			}
+		}
+	}
+
+	// Remove from DDNS routes if present
+	routeKey := fmt.Sprintf("%s/%s", namespace, name)
+	c.ddnsMutex.Lock()
+	delete(c.ddnsRoutes, routeKey)
+	c.ddnsMutex.Unlock()
+}
+
+// runDDNSJob runs a background job to check for IP changes in DDNS routes
+func (c *Controller) runDDNSJob() {
+	ticker := time.NewTicker(c.ddnsInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-c.ctx.Done():
+			return
+		case <-ticker.C:
+			c.ddnsMutex.RLock()
+			routes := make([]*ddnsRoute, 0, len(c.ddnsRoutes))
+			for _, route := range c.ddnsRoutes {
+				routes = append(routes, route)
+			}
+			c.ddnsMutex.RUnlock()
+
+			for _, ddnsRoute := range routes {
+				// Get the specific HTTPRoute
+				route, err := c.k8sClient.GetHTTPRoute(c.ctx, ddnsRoute.namespace, ddnsRoute.name)
+				if err != nil {
+					// Route might have been deleted, remove from tracking
+					log.Printf("Error getting HTTPRoute %s/%s for DDNS update (may have been deleted): %v", ddnsRoute.namespace, ddnsRoute.name, err)
+					c.ddnsMutex.Lock()
+					routeKey := fmt.Sprintf("%s/%s", ddnsRoute.namespace, ddnsRoute.name)
+					delete(c.ddnsRoutes, routeKey)
+					c.ddnsMutex.Unlock()
+					continue
+				}
+
+				c.processHTTPRoute(route, true) // true = isDDNSUpdate
+			}
+		}
+	}
+}
+
+// Helper funcs
+
+// getRecordNameFromHTTPRoute gets the first hostname from an HTTPRoute
+// TODO does this need to support multiple hostnames? For now, just one seems fine
+func getRecordNameFromHTTPRoute(route *unstructured.Unstructured) (string, error) {
+	hostnames, found, err := unstructured.NestedStringSlice(route.Object, "spec", "hostnames")
+	if !found || err != nil || len(hostnames) == 0 {
+		return "", fmt.Errorf("HTTPRoute has no hostnames in spec")
+	}
+	return hostnames[0], nil
+}
